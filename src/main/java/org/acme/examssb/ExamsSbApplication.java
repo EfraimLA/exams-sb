f4b6b780f66143778c1d78d6b238cd4dcbcc5bf6
Efraim LA <46459026+EfraimLA@users.noreply.github.com>
2022-07-15 16:45:36 -0500

    spring init

---
+++ src/main/java/org/acme/examssb/ExamsSbApplication.java
@@ -0,0 +1,13 @@
+package org.acme.examssb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamsSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamsSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/acme/examssb/ExamsSbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.acme.examssb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamsSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
